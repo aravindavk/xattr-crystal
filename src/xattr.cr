82e5025a81e5ec1c54ee1e113dd02529f0b2903b
Aravinda Vishwanathapura <mail@aravindavk.in>
2022-01-12 12:46:05 +0530

    Fix File and Dir methods
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

--- src/xattr.cr
+++ src/xattr.cr
@@ -5,15 +5,15 @@ module Xattr
 end
 
 module FileDirXattrMethods
-  def get(name : String, no_follow = false)
+  def getxattr(name : String, no_follow = false)
     Xattr.get(@path, name, no_follow: no_follow)
   end
 
-  def set(name : String, value : String, no_follow = false, only_create = false, only_replace = false)
+  def setxattr(name : String, value : String, no_follow = false, only_create = false, only_replace = false)
     Xattr.set(@path, name, value, no_follow: no_follow, only_create: only_create, only_replace: only_replace)
   end
 
-  def remove(name : String, no_follow = false)
+  def removexattr(name : String, no_follow = false)
     Xattr.remove(@path, name, no_follow: no_follow)
   end
 
